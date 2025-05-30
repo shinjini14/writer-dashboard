51426fa427aa8abe9da23d2fe27bd051a407608c
PlotAnalytics <analytics@plotpointe.com>
2025-05-30 09:53:20 +0530

    updated backend

--- api/auth/login.js
+++ api/auth/login.js
@@ -9,7 +9,7 @@ const users = [
     password: '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
     name: 'Steven Abreu',
     writerId: 74,
-    avatar: 'Steven'
+    avatar: 'S'
   }
 ];
 
